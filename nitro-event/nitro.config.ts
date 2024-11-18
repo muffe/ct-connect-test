44da39773a3cee218d5f1ceecdb9e47a1aae2077
Marc Wustrack <m.wustrack@basecom.de>
2024-11-18 17:36:30 +0100

    prettier

--- nitro-event/nitro.config.ts
+++ nitro-event/nitro.config.ts
@@ -1,9 +1,9 @@
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
-  srcDir: "server",
-  compatibilityDate: "2024-11-18",
+  srcDir: 'server',
+  compatibilityDate: '2024-11-18',
   runtimeConfig: {
     emailUser: '',
     emailPassword: '',
-  }
+  },
 });
